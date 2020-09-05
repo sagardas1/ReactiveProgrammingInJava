75717e8a1eba876c8aed1741b0197ae5509d1870
sagardas1 <sagardas.195@gmail.com>
2020-09-05 22:19:55 +0530

    changes

--- ReactiveProgramming/src/main/java/com/reactiveProgramming/controller/MyService.java
+++ ReactiveProgramming/src/main/java/com/reactiveProgramming/controller/MyService.java
@@ -10,25 +10,20 @@ import com.reactiveProgramming.repo.ProductDAO;
 @Service
 public class MyService {
 
-	
 	@Autowired
 	private ProductDAO myDao;
-	public Product getProductById(long id) {
-	List<Product> product=myDao.getProductById(id);
-	
-	
 
-	
-	
+	public Product getProductById(long id) {
+		List<Product> product = myDao.getProductById(id);
+
 		return product.get(0);
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		@SuppressWarnings("unused")
-		int a[]= {1,2,3,2,3,2,4,5};
-		
+		int a[] = { 1, 2, 3, 2, 3, 2, 4, 5 };
+
 	}
 
 }
