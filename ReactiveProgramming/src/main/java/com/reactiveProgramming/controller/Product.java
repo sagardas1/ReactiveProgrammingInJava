c00bf3029ab6a0d099498f045545b65989e81dc2
sagardas1 <sagardas.195@gmail.com>
2020-10-07 22:35:35 +0530

    changes

--- ReactiveProgramming/src/main/java/com/reactiveProgramming/controller/Product.java
+++ ReactiveProgramming/src/main/java/com/reactiveProgramming/controller/Product.java
@@ -5,30 +5,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long productId;
 	private String productName;
-	
-	
-	
+
 	public long getProductId() {
 		return productId;
 	}
+
 	public void setProductId(long productId) {
 		this.productId = productId;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-	
-	
 
 }
